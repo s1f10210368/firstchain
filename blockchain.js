665fb1fdd1a6df77b7fc96cfbe26524acb6824a6
s1f10210368 <s1f102103684@iniad.org>
2024-05-27 15:14:42 +0900

    R

--- blockchain.js
+++ blockchain.js
@@ -1,8 +1,17 @@
+const SHA256 = require('crypto-js/sha256');
+
+// ブロックを作成
 class Block {
     constructor(timestamp, data, previousHash) {
+        // 以下でブロック内に必要なタイムスタンプ、取引データ、1つ前のハッシュ、計算したハッシュ値を作成
         this.timestamp = timestamp;
         this.data = data;
-        //STEP1: previousHashのコンストラクタを追記
         this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+    // ハッシュ値を計算するためのメソッド
+    // dataは配列データのためJSONを使用
+    calculateHash() {
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
